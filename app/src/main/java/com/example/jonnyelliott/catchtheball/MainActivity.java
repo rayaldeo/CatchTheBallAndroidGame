6c53d08e025369889c4d446b9720e42d1ec63fc0
Jonathan Elliott <jelliot@peoplenetonline.com>
2017-01-08 20:21:16 -0600

    Added SkyBox

--- app/src/main/java/com/example/jonnyelliott/catchtheball/MainActivity.java
+++ app/src/main/java/com/example/jonnyelliott/catchtheball/MainActivity.java
@@ -3,24 +3,26 @@ package com.example.jonnyelliott.catchtheball;
 import android.content.Intent;
 import android.graphics.Point;
 import android.graphics.Typeface;
-import android.net.Uri;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.TranslateAnimation;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -29,6 +31,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView scoreLabel;
     private TextView startLabel;
     private ImageView box, fish, fly, slime;
+    private ImageView skyBox;
+    private Animation mAnimation;
 
     //Initialize  Class
     private Handler handler = new Handler();
@@ -44,6 +48,12 @@ public class MainActivity extends AppCompatActivity {
     private int slimeX;
     private int slimeY;
 
+    //Speed
+    private int boxSpeed;
+    private int fishSpeed;
+    private int flySpeed;
+    private int slimeSpeed;
+
     //Score
     private int score =0;
 
@@ -57,6 +67,10 @@ public class MainActivity extends AppCompatActivity {
     private int boxSize;
 
     private int screenHeight, screenWidth;
+
+    private ArrayList<Integer> skybiomes;
+    private int thisBiome =0;
+    private Random rand = new Random();
 
     boolean running;
 
@@ -75,6 +89,7 @@ public class MainActivity extends AppCompatActivity {
         fly = (ImageView) findViewById(R.id.fly);
         slime = (ImageView) findViewById(R.id.slim);
 
+
         //Get screen size
         WindowManager wm = getWindowManager();
         Display disp = wm.getDefaultDisplay();
@@ -83,6 +98,30 @@ public class MainActivity extends AppCompatActivity {
 
         screenWidth = size.x;
         screenHeight = size.y;
+
+        //Add All biomes to an ArrayList
+        skybiomes =new ArrayList<>();
+        skybiomes.add(R.drawable.rusted_sky);  skybiomes.add(R.drawable.violet_sky);
+        thisBiome = rand.nextInt((skybiomes.size() - 1) + 1);
+        //Set the SkyBox For the Beginning of the Game
+        //setSkyBox(skybiomes.get(thisBiome));
+        setSkyBox(skybiomes.get(thisBiome));
+
+
+        ///
+        //Now
+        //Nexus witdth:768 height:1184
+        //Spped box:20 fish:12 fly:20 slime:16
+        boxSpeed = Math.round(screenHeight/60F);//1184/60 =19.733...=>20
+        fishSpeed = Math.round(screenWidth/60F);//768/60 =12.8...=>13
+        flySpeed = Math.round(screenWidth/36F);//768/36 =21.333...=>21
+        slimeSpeed = Math.round(screenWidth/46F);//768/45 =17.06...=>17
+
+
+        Log.v("SPEED_BOX",boxSpeed+"");
+        Log.v("SPEED_FISH",fishSpeed+"");
+        Log.v("SPEED_FLY",flySpeed+"");
+        Log.v("SPEED_SLIME",slimeSpeed+"");
 
         //Move to out of Screen
         fish.setX(getResources().getDimension(R.dimen.starting_position));
@@ -111,7 +150,6 @@ public class MainActivity extends AppCompatActivity {
             boxY =(int)box.getY();
             //The box is a square.(height and width are the same)
             boxSize = box.getHeight();
-
             startLabel.setVisibility(View.GONE);
             timer.schedule(
                     new TimerTask(){
@@ -137,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void changePos(){
         hitCheck();
-        fishX -=12;
+        fishX -=fishSpeed;
         if(fishX<0){
             fishX = screenWidth+20;
             fishY = (int)Math.floor(Math.random()* (frameHeight - fish.getHeight()));
@@ -146,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
         fish.setX(fishX);
         fish.setY(fishY);
 
-        flyX -= 16;
+        flyX -= flySpeed;
         if(flyX<0){
             flyX = screenWidth+20;
             flyY = (int)Math.floor(Math.random()* (frameHeight - fish.getHeight()));
@@ -155,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
         fly.setX(flyX);
         fly.setY(flyY);
 
-        slimeX -= 16;
+        slimeX -= slimeSpeed;
         if(slimeX<0){
             slimeX = screenWidth+5000;
             slimeY = (int)Math.floor(Math.random()* (frameHeight - fish.getHeight()));
@@ -166,10 +204,10 @@ public class MainActivity extends AppCompatActivity {
 
         //Move Box
         if(action_flg){
-            boxY -=20;
+            boxY -=boxSpeed;
         }else{
             //Releasing
-           boxY+=20;
+           boxY+=boxSpeed;
         }
         if(boxY<0) boxY=0;
         if(boxY> frameHeight - boxSize)boxY = frameHeight-boxSize;
@@ -213,9 +251,12 @@ public class MainActivity extends AppCompatActivity {
 
         if(0<= slimeCenterX && slimeCenterX <= boxSize
                 && boxY <=slimeCenterY&& slimeCenterY <= boxY+boxSize){
+
+            //mAnimation.cancel();
+            //mAnimation.reset();
             //Stop Timer
             timer.cancel();
-            timer=null;
+            //timer=null;
             sound.playOverSound();
             //Show Result
             Intent intent = new Intent(getApplicationContext(),Result.class);
@@ -236,6 +277,29 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         return super.dispatchKeyEvent(event);
+    }
+
+    public void setSkyBox(int icon){
+        //Setting up the Infinite SkyBox
+        skyBox = (ImageView) findViewById(R.id.skybox);
+        skyBox.setImageResource(icon);
+        final int skyBoxWidth = ContextCompat.getDrawable(this, icon).getIntrinsicWidth();
+        int animatedViewWidth = 0;
+        while (animatedViewWidth < screenWidth) {
+            animatedViewWidth += skyBoxWidth;
+        }
+        animatedViewWidth += skyBoxWidth;
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) skyBox.getLayoutParams();
+        layoutParams.width = animatedViewWidth;
+        skyBox.setLayoutParams(layoutParams);
+
+        //Move the Image to the Left
+        final int repeatingBackGroundDrawable = ContextCompat.getDrawable(this, R.drawable.repeating_background).getIntrinsicWidth();
+        mAnimation = new TranslateAnimation(0, -repeatingBackGroundDrawable, 0, 0);
+        mAnimation.setInterpolator(new LinearInterpolator());
+        mAnimation.setRepeatCount(Animation.INFINITE);
+        mAnimation.setDuration(2500);
+        skyBox.startAnimation(mAnimation);
     }
 
     public void onDestroy(){
